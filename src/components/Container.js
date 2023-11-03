7e6a140ee5976b740e2228fb102a1a56f0b31929
Chris Schultz <cschultz@opentext.com>
2023-11-03 09:04:44 -0400

    removed console log

--- src/components/Container.js
+++ src/components/Container.js
@@ -63,8 +63,6 @@ export default function Container () {
 
     cushions.push(...veganCushions);
 
-    console.log("Cushions Check", cushions);
-
     const shopifyData = {
         cushions,
         placemats,
